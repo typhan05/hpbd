639758506eaf71c3c4272f818cff0625a675e841
Ty Phan <typhan.vn@gmail.com>
2022-11-15 18:19:19 +0700

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,7 @@
   function typingHappy() {
     var options = {
       strings: ['Có được vợ trong cuộc đời này là một điều thật tuyệt vời và may mắn nhất đối với anh. Cảm ơn vợ đã luôn ở bên ủng hộ và quan tâm anh. Chúc vợ meo yêu có một ngày sinh nhật tuyệt vời, một tuổi mới với nhiều may mắn, sức khoẻ, thành công và ngày càng xinh đẹp vợ nhé!'],
+      typeSpeed: 30
     };
     var typed = new Typed('#intro', options);
   }
@@ -10,16 +11,16 @@
     setTimeout(function() {
       $('.section-2').addClass('show');
       $('#myVideo').get(0).pause();
-    }, 10000);
+    }, 25000);
   }
   function countDown() {
     var countdownNumberEl = document.getElementById('countdown-number');
-    var countdown = 10;
+    var countdown = 25;
 
     countdownNumberEl.textContent = countdown;
 
     setInterval(function() {
-      countdown = --countdown <= 0 ? 10 : countdown;
+      countdown = --countdown <= 0 ? 25 : countdown;
 
       countdownNumberEl.textContent = countdown;
     }, 1000);
